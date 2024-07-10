9152998ab7c7e275480ea7b093b93ff7ba2f46f9
TorreiroW <githubwto@toorren.net>
2024-07-10 08:31:47 +0200

    v3.0.0

---
+++ default.nix
@@ -0,0 +1,96 @@
+{
+  lib,
+  buildPythonPackage,
+  click,
+  click-completion,
+  factory-boy,
+  faker,
+  fetchPypi,
+  inquirer,
+  notify-py,
+  pbr,
+  pendulum,
+  prettytable,
+  pytest-mock,
+  pytestCheckHook,
+  pythonOlder,
+  requests,
+  setuptools,
+  twine,
+  validate-email,
+}:
+
+buildPythonPackage rec {
+  pname = "toggl-cli";
+  version = "3.0.0";
+  pyproject = true;
+
+  disabled = pythonOlder "3.7";
+
+  src = fetchPypi {
+    pname = "togglCli";
+    inherit version;
+    hash = "sha256-P4pv6LMPIWXD04IQw01yo3z3voeV4OmsBOCSJgcrZ6g=";
+    hash = "sha256-F23E/bovDA7KFGhgCk1gsf6BA2V7Voc9WyWrYBR0hrM=";
+  };
+
+  postPatch = ''
+    substituteInPlace requirements.txt \
+      --replace-fail "==" ">="
+    substituteInPlace pytest.ini \
+      --replace ' --cov toggl -m "not premium"' ""
+  '';
+
+  build-system = [
+    pbr
+    setuptools
+    twine
+  ];
+
+  dependencies = [
+    click
+    click-completion
+    inquirer
+    notify-py
+    pbr
+    pendulum
+    prettytable
+    requests
+    validate-email
+  ];
+
+  nativeCheckInputs = [
+    pytestCheckHook
+    pytest-mock
+    faker
+    factory-boy
+  ];
+
+  preCheck = ''
+    export TOGGL_API_TOKEN=your_api_token
+    export TOGGL_PASSWORD=toggl_password
+    export TOGGL_USERNAME=user@example.com
+  '';
+
+  disabledTests = [
+    "integration"
+    "premium"
+    "test_basic_usage"
+    "test_now"
+    "test_parsing"
+    "test_type_check"
+  ];
+
+  pythonImportsCheck = [ "toggl" ];
+
+  # updates to a bogus tag
+  passthru.skipBulkUpdate = true;
+
+  meta = with lib; {
+    description = "Command line tool and set of Python wrapper classes for interacting with toggl's API";
+    homepage = "https://toggl.uhlir.dev/";
+    license = licenses.mit;
+    maintainers = with maintainers; [ mmahut ];
+    mainProgram = "toggl";
+  };
+}
